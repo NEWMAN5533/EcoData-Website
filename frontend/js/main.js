2efe16e24368d04ebac190db4da308f200382673
NEWMAN5533 <ilyasahmed553341@gmail.com>
2025-10-16 13:18:13 +0000

    Add files via upload

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,3 +8,23 @@ onAuthStateChanged(auth, (user) => {
     console.log("✅ User logged in:", user.email);
 }
 });
+
+
+
+// === LOAD STATUS ===
+async function loadStatus() {
+  try {
+    const res = await fetch("https://ecodata-app.onrender.com/api/v1/orders/status");
+    const data = await res.json();
+    if (data.success && data.data) {
+      document.getElementById("pendingCount").innerText = data.data.pending;
+      document.getElementById("processingCount").innerText = data.data.processing;
+      document.getElementById("completedCount").innerText = data.data.complete;
+      document.getElementById("failedCount").innerText = data.data.failed;
+    }
+  } catch (err) {
+    console.error("Error fetching status:", err);
+  }
+}
+loadStatus();
+setInterval(loadStatus,3000);
