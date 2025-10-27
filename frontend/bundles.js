10b6fb21964ccb40addee2e624d6bce5513795f9
NEWMAN5533 <ilyasahmed553341@gmail.com>
2025-10-27 17:20:27 +0000

    Add files via upload

--- frontend/bundles.js
+++ frontend/bundles.js
@@ -5,7 +5,7 @@
         <li>MTN Bundles are delivered within 2–30minutes after it is prompted as delivered at the section for checking order status.</li>
         <li>Ensure your number starts with <b> 23355123..., 23359123..., or 23325123..</b>.</li>
         <li>If delivery delays, contact 
-        <p><strong>WhatsAAp:</strong> 0535565637</p></li>
+        <p><strong>WhatsAp:</strong> 0535565637</p></li>
         <li>All transactions are secured via Paystack.</li>
       </ul>
       <p><b>Refunds:</b> If bundle purchase failed, refunds are processed within 24 hours.</p>
