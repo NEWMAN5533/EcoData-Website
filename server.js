ffeb436625cf4ead427baf579b55bb3c01c73400
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-03-03 22:30:01 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ import admin from "firebase-admin";
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FB_PROJECT_ID,
-    clientEmail: process.env.FB_CLIENT-EMAIL,
+    clientEmail: process.env.FB_CLIENT_EMAIL,
     privateKey: process.env.FB_PRIVATE_KEY.replace(/\\n/g,'\n'),
   }),
 });
