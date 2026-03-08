33acbdb848fbadd78a3b1b7eb7c6a81d1d665d58
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-03-08 10:27:46 +0000

    Add files via upload

--- frontend/bundles.js
+++ frontend/bundles.js
@@ -1,32 +1,44 @@
   const bundleDetails = {
     mtn: `
-      <h3>MTN Bundle Process</h3>
-      <ul>
+      <h3 class="htu">MTN Bundle Process</h3>
+      <div class="uli">
+         <ul>
         <li>MTN Bundles are delivered within 2–30minutes after it is prompted as delivered at the section for checking order status.</li>
-        <li>Ensure your number starts with <b> 055123..., 059123..., or 025123..</b>.</li>
+        <li class=li>Ensure your number starts with <b> 23355123..., 23359123..., or 23325123..</b>.</li >
         <li>If delivery delays, contact 
         <p><strong>WhatsAp:</strong> 0535565637</p></li>
         <li>All transactions are secured via Paystack.</li>
       </ul>
+      
+    
+   
       <p><b>Refunds:</b> If bundle purchase failed, refunds are processed within 24 hours.</p>
-    `,
+      
+      </div>`,
     vodafone: `
-      <h3>Telecel Bundle Process</h3>
+      <h3 class="hvu">Telecel Bundle Process</h3>
+      <div class="uli">
       <ul>
         <li>Bundles are delivered within 2–30minutes after successful payment.</li>
-        <li>Ensure your number starts with <b>02012345... or 0501234...</b>.</li>
+        <li>Ensure your number starts with <b>2332012345... or 233501234...</b>.</li>
         <li>For failed delivery, contact support immediately.</li>
       </ul>
+
       <p><b>Refunds:</b> Verified failed transactions will be refunded within 24 hours.</p>
+      </div>
     `,
     airteltigo: `
-      <h3>AirtelTigo Bundle Process</h3>
+      <h3 class="hto">AirtelTigo Bundle Process</h3>
+
+      <div class="uli">
+     
       <ul>
         <li>Delivery is automatic once payment is verified.</li>
-        <li>Ensure your number starts with <b>026..., 056..., or 027...</b>.</li>
+        <li>Ensure your number starts with <b>23326..., 23356..., or 23327...</b>.</li>
         <li>Keep your account active to avoid delay.</li>
       </ul>
-      <p><b>Refunds:</b> If unsuccessful, report within 24 hours for a quick refund.</p>
+      <p><b>Refunds:</b> If unsuccessful, report within 24 hours for a quick refund.</p> </div>
+     
     `
   };
 
