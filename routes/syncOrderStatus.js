2af8922e72ff86910a05267ce9c21ec7af649d29
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-05-30 14:50:28 +0000

    Update syncOrderStatus.js

--- routes/syncOrderStatus.js
+++ routes/syncOrderStatus.js
@@ -1,6 +1,6 @@
 import express from "express";
 import axios from "axios";
-import { db } from "../firebaseAdmin.js";
+import { admin, db } from "../firebaseAdmin.js";
 
 const syncOrderRoute = express.Router();
 
