4332ceecda050450d6427600a06c05498287f32c
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-05-31 20:17:14 +0000

    Add files via upload

---
+++ frontend/userRestriction.js
@@ -0,0 +1,210 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
+import { signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClNBlfigtQk8AZWdMZcU9sEtVcIrS0D1g",
+  authDomain: "ecodata-2bee6.firebaseapp.com",
+  projectId: "ecodata-2bee6",
+  storageBucket: "ecodata-2bee6.firebasestorage.app",
+  messagingSenderId: "544837123249",
+  appId: "1:544837123249:web:6c362350a00c6dab10b690"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, async (user) => {
+  const usernameDisplay = document.getElementById("usernameDisplay");
+
+      const greeting = updateClock();
+
+      if (user) {
+      const userSnap = await getDoc(doc(db, "users", user.uid));
+      usernameDisplay.textContent = `${greeting}, ${userSnap.data().username}!`;
+    } else {
+      usernameDisplay.textContent = `${greeting}, Dear `;
+    }
+
+
+
+
+  try {
+
+    if(!user) return;
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (!userSnap.exists()) return;
+
+    const userData = userSnap.data();
+
+   if (
+  userData.isAgent === true &&
+  userData.isAdmin !== true
+){
+  window.location.href = "agentPage.html";
+  return;
+}
+
+   
+     const adminLink1 = document.getElementById("adminAccessLink1");
+     const adminLink2 = document.getElementById("adminAccessLink2");
+     const adminLink3 = document.getElementById("adminAccessLink3");
+     const adminLink4 = document.getElementById("adminAccessLink4");
+
+      // Admin  access
+   const adminLinks = [
+  adminLink1,
+  adminLink2,
+  adminLink3,
+  adminLink4
+];
+
+if (userData.isAdmin === true) {
+
+  adminLinks.forEach(link => {
+    link.style.display = "flex";
+  });
+
+}
+
+  } catch (err) {
+    console.error("Error fetching user data:", err);
+  }
+});
+
+
+
+const lapOutBtn = document.getElementById("logOut");
+const logOutBtn = document.getElementById("lapOut");
+
+if(logOutBtn){
+  logOutBtn.addEventListener("click", async() => {
+    try {
+      await signOut(auth);
+      showSnackBar("Logged out successfully!");
+    } catch(err){
+      console.error("LogOut error;", err);
+      showSnackBar("Error logging out.", "warning");
+    }
+  })
+}
+
+
+// laptop logout
+if(lapOutBtn) {
+ 
+  lapOutBtn.addEventListener("click", async() => {
+
+    try {
+      await signOut(auth);
+      showSnackBar("Logged out successfully!");
+    } catch (error) {
+      console.error("LogOut error:", error);
+      showSnackBar("Error logging out.");
+    }
+  });
+}
+
+
+
+
+// SIGN OR LOGIN POP UP
+
+onAuthStateChanged(auth, (user)=>{
+
+  const seen = localStorage.getItem("signupPromptSeen");
+
+  if(!user && !seen){
+    setTimeout(()=>{
+      showSignupModal();
+    },2000);
+  }
+
+});
+
+
+  const signupModal = document.getElementById("signupModal");
+const closeSignup = document.getElementById("closeSignup");
+
+function showSignupModal(){
+  signupModal.classList.add("active");
+}
+
+function hideSignupModal(){
+  signupModal.classList.remove("active");
+}
+
+closeSignup.addEventListener("click", ()=>{
+  hideSignupModal();
+  localStorage.setItem("signupPromptSeen","yes");
+});
+
+
+document.getElementById("createAccountBtn").onclick = ()=>{
+  window.location.href = "./signUp.html";
+};
+
+document.getElementById("loginBtn").onclick = ()=>{
+  window.location.href = "./login.html";
+};
+
+// FORCE TO LOGIN END
+
+
+
+
+  
+  
+
+// SNACKBAR SECTION //
+// ===== SNACKBAR FUNCTION ===== //
+let snackbarTimeout = null;
+
+function showSnackBar(message, type = "info", duration = 4000) {
+  let snackbar = document.querySelector(".snackbar");
+
+  // Create snackbar if it doesn't exist
+  if (!snackbar) {
+    snackbar = document.createElement("div");
+    snackbar.className = "snackbar";
+
+    snackbar.innerHTML = `
+      <span class="snackbar-text"></span>
+      <div class="snackbar-progress"></div>
+    `;
+
+    document.body.appendChild(snackbar);
+  }
+
+  // Update text
+  snackbar.querySelector(".snackbar-text").textContent = message;
+
+  // Color by type
+  if (type === "success") snackbar.style.background = "rgba(7, 29, 26, 0.95)";
+  else if (type === "error") snackbar.style.background = "#88353f";
+  else if (type === "warning") snackbar.style.background = "#413b2a";
+  else snackbar.style.background = "rgba(7, 29, 26, 0.95)";
+
+  // Reset progress animation
+  const progress = snackbar.querySelector(".snackbar-progress");
+  progress.style.animation = "none";
+  void progress.offsetWidth;
+  progress.style.animation = `snackbar-progress ${duration}ms linear forwards`;
+
+  snackbar.classList.add("show");
+
+  // Clear previous timeout
+  if (snackbarTimeout) clearTimeout(snackbarTimeout);
+
+  snackbarTimeout = setTimeout(() => {
+    snackbar.classList.remove("show");
+  }, duration);
+}
+// snackbar ends
+
+
