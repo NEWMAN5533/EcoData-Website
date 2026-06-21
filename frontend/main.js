76bdecbc3b796031cf808549f1b81102afa44874
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-06-21 10:53:06 +0000

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -51,13 +51,19 @@ const PACKAGE_MAP = {
 
 const STORAGE_KEY = "selected_mtn_package";
 
+
+
+
 function resolvePackageName(network) {
+
   if (network === "mtn") {
     return selectedPackageName;
   }
 
   if (network === "at") {
     return "ishare_data_bundle";
+
+
   }
 
   if (network === "telecel") {
@@ -171,7 +177,7 @@ const mtnAreaDiv = document.getElementById("bundles");
      mashTopRightContainerh2.textContent = "MTN Data Bundle";
   mashTopRightContainerSmall.textContent = "Purchase MTN data bundles for single or multiple recipients";
 
-  mashTopRightContainerBtn.textContent = "17 Packages Available";
+  mashTopRightContainerBtn.textContent = "16 Packages Available";
 
       // resize the dropdown container
     flexingContainer.style.height = "28rem";
@@ -182,7 +188,7 @@ const mtnAreaDiv = document.getElementById("bundles");
     mashTitle.textContent = "MTN DATA BUNDLE";
 
     // packs to
-    mashPacks.textContent = "17 Packages Available";
+    mashPacks.textContent = "16 Packages Available";
    
     // change labelRecipient to
     mashRecipient.textContent = "Bundle Recipient Number";
@@ -321,7 +327,6 @@ function resetSelectedBundle(reason = "") {
     <p class='doneSelect'> <img src="./css/icons/done (2).png" ></p>
     <p class='doneSelect darkM'> <img src="./css/icons/done (1).png" ></p>
     <span><img src="./css/icons/more.png.png"></span>`;
-
   };
 
 
@@ -483,7 +488,31 @@ document.addEventListener("click", () => {
 
           lastPurchasedBundle = bundle; // 🔑 SAVE FOR POST-PAYMENT
 
-          
+        
+            // TELECEL NETWORK BG
+            if(button.dataset.network === "telecel"){
+              document.querySelector(".modal-content-top").style.background = 
+              "linear-gradient(to right, rgb(241, 62, 122), rgb(192, 3, 82))";
+
+              document.querySelector(".continue").style.background = 
+              "linear-gradient(to right, rgb(241, 62, 122), rgb(192, 3, 82))";
+
+
+              document.getElementById("networkTag").style.background =
+              "linear-gradient(to right, rgb(241, 62, 122), rgb(192, 3, 82))";
+            }
+
+            // AT NETWORK BG
+              if(button.dataset.network === "at"){
+              document.querySelector(".modal-content-top").style.background = 
+              " linear-gradient(to right, rgb(75, 148, 233), rgb(4, 145, 170))";
+
+              document.querySelector(".continue").style.background = 
+              " linear-gradient(to right, rgb(75, 148, 233), rgb(4, 145, 170))";
+
+              document.getElementById("networkTag").style.background =
+              "linear-gradient(to right, rgb(75, 148, 233), rgb(4, 145, 170))";
+            }
 
           // Update modal preview
           document.getElementById("priceTag").textContent =
