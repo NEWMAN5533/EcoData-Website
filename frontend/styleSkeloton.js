0a03b80a98e504525eabe6bd8e3045fe4ce98945
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-06-29 01:48:56 +0000

    Add files via upload

--- frontend/styleSkeloton.js
+++ frontend/styleSkeloton.js
@@ -23,6 +23,15 @@ document.addEventListener("DOMContentLoaded", ()=> {
     fadeSkeleton('sk-NavBar', 3000);
     fadeSkeleton('sk-Welcome', 5000);
     fadeSkeleton('sk-Trusted', 10050);
+    fadeSkeleton('sk-info', 10050);
+    fadeSkeleton('offer-sk', 10050);
     fadeSkeleton('sk-Notice', 7000);
+    fadeSkeleton('sk-gallery', 7000);
+    fadeSkeleton('sk-follow-share', 7000);
+    fadeSkeleton('sk-daily', 7000);
+    fadeSkeleton('sk-totals', 7000);
+    fadeSkeleton('sk-table', 9000);
+    fadeSkeleton('sk-mode-btn', 19000);
+    fadeSkeleton('sk-normalView', 19000);
    });
 });
