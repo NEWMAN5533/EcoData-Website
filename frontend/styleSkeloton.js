1c8a9aa23a8115c9e17811df878e934089a5aa66
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-06-29 02:04:43 +0000

    Update styleSkeloton.js

--- frontend/styleSkeloton.js
+++ frontend/styleSkeloton.js
@@ -21,17 +21,17 @@ document.addEventListener("DOMContentLoaded", ()=> {
    // ===============
    window.addEventListener('load', () => {
     fadeSkeleton('sk-NavBar', 3000);
-    fadeSkeleton('sk-Welcome', 5000);
+    fadeSkeleton('sk-Welcome', 6000);
     fadeSkeleton('sk-Trusted', 10050);
     fadeSkeleton('sk-info', 10050);
     fadeSkeleton('offer-sk', 10050);
     fadeSkeleton('sk-Notice', 7000);
-    fadeSkeleton('sk-gallery', 7000);
+    fadeSkeleton('sk-gallery', 10050);
     fadeSkeleton('sk-follow-share', 7000);
-    fadeSkeleton('sk-daily', 7000);
+    fadeSkeleton('sk-daily', 10050);
     fadeSkeleton('sk-totals', 7000);
-    fadeSkeleton('sk-table', 9000);
-    fadeSkeleton('sk-mode-btn', 19000);
-    fadeSkeleton('sk-normalView', 19000);
+    fadeSkeleton('sk-table', 10000);
+    fadeSkeleton('sk-mode-btn', 13000);
+    fadeSkeleton('sk-normalView', 12000);
    });
 });
