8c9e2612210930af47cfed79762c4ac5f14cae7a
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-07-20 12:46:48 +0000

    Add files via upload

--- frontend/styleSkeloton.js
+++ frontend/styleSkeloton.js
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
    window.addEventListener('load', () => {
     fadeSkeleton('sk-NavBar', 3000);
     fadeSkeleton('sk-Welcome', 6000);
-    fadeSkeleton('sk-Trusted', 6050);
+    fadeSkeleton('sk-Trusted', 50);
     fadeSkeleton('sk-info', 6050);
     fadeSkeleton('sk-deliverTracker1', 6050);
     fadeSkeleton('sk-deliverTracker2', 6050);
