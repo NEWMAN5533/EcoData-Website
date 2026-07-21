a0e4d8832d29d318e78ee0237e1a1effbb2768ce
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-07-21 13:10:43 +0000

    Add files via upload

---
+++ routes/validateRecipient.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import axios from 'axios';
+
+
+const validateRecipientRoute = express.Router();
+
+validateRecipientRoute.post("/", async (req, res) => {
+  try {
+    const { phone, offerSlug } = req.body;
+
+    if (!phone || offerSlug) {
+      return res.status(400).json({
+        success: false,
+        message: "Phone number and offerSlug are required."
+      });
+    }
+
+    const response = await fetch("https://swiftdata-link.com/api/v1/orders/validate-recipient",
+      {
+        method: 'POST',
+        headers: {
+          "content-Type": "application/json",
+          "x-api-key": process.env.SWIFT_API_KEY
+        },
+        body: JSON.stringify({
+          phone,
+          network: "MTN",
+          offerSlug,
+          skipCache: true
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    res.json(data);
+  } catch (error) {
+    console.error("Recipient validation error:", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Unable to validate recipient."
+    });
+  }
+});
+
+export default validateRecipientRoute;
