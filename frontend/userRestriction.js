2c11a605edda3911aa95bf1f9dea4a8cd6bdce2c
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-07-26 06:42:52 +0800

    Add files via upload

--- frontend/userRestriction.js
+++ frontend/userRestriction.js
@@ -4,42 +4,53 @@ import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/
 import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 import { signOut } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
+import { firebaseConfig } from "./firebase.js";
 
-// firebaseConfig 
-import { firebaseConfig } from "./all_js/firebase.js";
+// import exported functions
+import { updateClock } from "./clock";
+import { hideSignupModal } from "./modal.js";
+import { showSignupModal } from "./modal.js";
+import { showSnackBar } from "./snackbar.js";
+import { logoutUser } from "./logOutUser.js";
+import { getUserData } from "./user.js";
+import { updateUserUI } from "./ui.js";
+import { updateAccess } from "./ui.js";
 
-// import firebase config
-import { showSignupModal } from "./all_js/modal.js";
-import { hideSignupModal } from "./all_js/modal.js";
-import { showGuestUI } from "./all_js/ui.js";
-import { updateUserUI } from "./all_js/ui.js";
-import { updateAccess } from "./all_js/ui.js";
-import { updateClock } from "./all_js/clock.js";
-import { getUserData } from "./all_js/user.js";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
 
 
 
 onAuthStateChanged(auth, async (user) => {
 
+  const usernameDisplay = document.getElementById("usernameDisplay");
+  const ifUserName = document.getElementById("ifUserName");
+  const helloName = document.getElementById("helloName");
+  const helloContent = document.getElementById("helloContent");
+
   const greeting = updateClock();
 
-  if(!user) {
-    showGuestUI(greeting);
+if (user) {
+      const userSnap = await getDoc(doc(db, "users", user.uid));
+      const userRef = doc(db, "users", user.uid);
+      const userSnap = await getDoc(userRef);
+      const userData = userSnap.data();
+      if(!user) return;
+
+      if (!userSnap.exists()) return;
+
+      updateUserUI(userData, greeting);
+
+      updateAccess(userData);
+
+    }
+    
+    logoutUser(auth);
+
     showSignupModal();
-    return;
-  }
 
+  });
 
-  const userData = await 
-  getUserData(user.uid);
-
-  if(!userData) return;
-
-  updateUserUI(userData, greeting);
-
-  updateAccess(userData);
-  
-
-  hideSignupModal();
-  showSignupModal();
-});
