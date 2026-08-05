6bf57d6e3405ba0301f01538a13cc840581ab54d
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-08-06 01:59:05 +0800

    Change sync interval from 10 to 9 minutes

--- cron/syncOrders.js
+++ cron/syncOrders.js
@@ -5,7 +5,7 @@ import { db } from "../firebaseAdmin.js";
 export function startOrderSyncJob() {
 
   // Every 10 minutes
-  cron.schedule("*/7 * * * *", async () => {
+  cron.schedule("*/9 * * * *", async () => {
 
     console.log("🔄 Running Swift sync job:", new Date());
 
