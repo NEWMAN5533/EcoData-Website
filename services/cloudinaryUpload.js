b6f803a9672e87f5004087f00ae3a587898924a9
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-08-12 16:35:04 +0800

    Add files via upload

---
+++ services/cloudinaryUpload.js
@@ -0,0 +1,25 @@
+import cloudinary from "../config/cloudinary.js";
+import { Readable } from "node:stream";
+
+export function uploadToCloudinary(buffer, options = {}){
+  return new Promise((resolve, reject ) => {
+    const uploadStream =
+    cloudinary.uploader.upload_stream({
+      resource_type: options.resource_type || "auto",
+      folder: options.folder || "ecodata/products", 
+      public_id: options.public_id,
+      type: options.type || "upload"
+    },
+    (error, result) => {
+      if(error){
+        reject(error);
+        return;
+      }
+      resolve(result);
+    }
+  );
+  Readable
+  .from(buffer)
+  .pipe(uploadStream);
+  });
+}
