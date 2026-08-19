6619731ae0da8161f8ba75144c40b678f541b06f
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-08-19 19:22:10 +0800

    Update cloudinaryUpload.js

--- services/cloudinaryUpload.js
+++ services/cloudinaryUpload.js
@@ -1,4 +1,4 @@
-import { cloudinary } from "../config/cloudinary.js";
+import  cloudinary  from "../config/cloudinary.js";
 import { Readable } from "node:stream";
 
 export function uploadToCloudinary(buffer, options = {}){
