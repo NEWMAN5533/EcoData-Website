debb9add04f1421edb7eff3f63ce4d212b55708c
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-08-19 19:17:18 +0800

    Update cloudinaryUpload.js

--- services/cloudinaryUpload.js
+++ services/cloudinaryUpload.js
@@ -2,18 +2,12 @@ import { cloudinary } from "../config/cloudinary.js";
 import { Readable } from "node:stream";
 
 export function uploadToCloudinary(buffer, options = {}){
-
   return new Promise((resolve, reject ) => {
-
     const uploadStream =
-    cloudinary.uploader.upload_stream(
-      {
+    cloudinary.uploader.upload_stream({
       resource_type: options.resourceType || "auto",
-
-      folder: options.folder || "ecodata/products",
- 
+      folder: options.folder || "ecodata/products", 
       public_id: options.publicId,
-
       type: options.type || "upload"
     },
     (error, result) => {
