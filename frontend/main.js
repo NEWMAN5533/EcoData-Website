03d04bdc23f47402a127724bd5d986ea41f3e28d
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-09-17 06:59:15 +0000

    Update main.js

--- frontend/main.js
+++ frontend/main.js
@@ -1017,9 +1017,9 @@ function activateTrackers() {
 
 // un activate trackers when no orders
 function unActivateTrackers(){
- const firstTracker = document.getElementById("deliveryTracker1").style.display = "flex";
+ const firstTracker = document.getElementById("deliveryTracker1").style.display = "none";
 
- const secondTracker = document.getElementById("deliveryTracker2").style.display = "flex";
+ const secondTracker = document.getElementById("deliveryTracker2").style.display = "none";
 };
 
 //=================================
@@ -2997,16 +2997,14 @@ function addOrderToLeaderboard(customers, order) {
     const customer =
         customers[phone];
 
-    const isAdminLeaderboard =
-    order.LD_Status === "leaderboard" ||
-    order.status === "leaderboard";
+   
 
 
     //========================================
     // NORMAL CUSTOMER ORDER
     //========================================
 
-    if(!isAdminLeaderboard){
+    
     const gb =
         Number(order.volume || 0);
 
@@ -3020,7 +3018,7 @@ function addOrderToLeaderboard(customers, order) {
 
     customer.totalSpent += amount;
 
-    }
+  
 
     //========================================
     // CHECK MANUAL POINTS FROM ADMIN
