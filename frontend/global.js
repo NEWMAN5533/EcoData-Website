066271fa8c6a13372698d7536bc3750707ce9b0c
NEWMAN5533 <ilyasahmed553341@gmail.com>
2026-09-25 11:28:11 +0000

    Add files via upload

--- frontend/global.js
+++ frontend/global.js
@@ -26,7 +26,7 @@ if(shoppingBtn){
     const totalCustomersOrders = document.getElementById("stat-num");
     if(totalCustomersOrders){
       totalCustomersOrders.textContent = 
-      "1.46K+";
+      "1.48K+";
     }
 
 
